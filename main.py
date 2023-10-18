48bba3ada1ac410c32b2307ac791954e320bb6ea
agusvaser <avaserman@itba.edu.ar>
2023-10-18 13:31:21 -0300

    geodatos
    Avance un toque con lo de los geodatos

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 # Import packages
 import pandas as pd
-from pandas_profiling import ProfileReport
+# from pandas_profiling import ProfileReport
+from shapely.geometry import Point
+from shapely.geometry import shape
+from shapely.geometry import MultiPolygon
 
 # Import data
 data_mediciones = pd.read_csv("acumar_mediciones.csv")
 
-# EDA report
-# profile = ProfileReport(data_mediciones)
-# profile.to_file("eda_mediciones_reporte.html")
+limites_subcuencas = pd.read_csv("LIMITES_SUBCUENCAS_.csv")
+
+# data_mediciones["Latitud"] = data_mediciones["Latitud"].str.replace(",",".")
+# data_mediciones["Longitud"] = data_mediciones["Latitud"].replace(",",".")
+# data_mediciones["Latitud"] = data_mediciones["Latitud"].astype(float)
+# data_mediciones["Longitud"] = data_mediciones["Latitud"].astype(float)
+# data_mediciones.to_csv("acumar_mediciones.csv",index=False)
+for medicion in data_mediciones:
+    print(medicion[3])
+
+
+
+
+for medicion in data_mediciones:
+    point = Point(medicion["Longitud"],medicion["Latitud"])
+    for subcuenca in limites_subcuencas:
+        polygon = shape(MultiPolygon.from_wkt(subcuenca[1]))
+        if point.within(polygon):
+            medicion["subcuenca"] = subcuenca[2]
