709d92886ff64699cf9d2fc9ce614105b16ec012
Ariel Waisburg <ariel.waisburg@celeri.app>
2023-10-20 13:10:19 -0300

    not working code improved

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Import packages
 import pandas as pd
 from shapely.geometry import Point
-from shapely.geometry import shape
-from shapely.geometry import MultiPolygon
+from shapely.wkt import loads
+import geopandas as gpd
 
 # Functions
 def parse_ia(ia_value):
@@ -12,12 +12,12 @@ def parse_ia(ia_value):
         return tuple(ia_value_range)
     return float(ia_value)
     
-def check_condition(row, tipo_uso):
+def check_dummie_condition(row, tipo_uso):
     medida = row['Medida']
     valor = row['Valor (=)']
     for _, ia in data_limites_uso.iterrows():
         if medida == ia['Parámetro'] and 'Aguas' in row['Análisis']:
-            ia_value = parse_ia(ia[tipo_uso]) # Porque aca directamente no usamos valor? te ahorras que use toda la funcion total la columna valor ya la limpiamos
+            ia_value = parse_ia(ia[tipo_uso])
             if isinstance(ia_value, tuple):
                 ia_min, ia_max = ia_value
                 return ia_min <= valor <= ia_max
@@ -58,31 +58,57 @@ def standardize_measurement(medida):
     }
     return standardized_measurements.get(medida, medida)
     
+def geomapping(point, gdf, column_geom, col_name):
+    for idx, valor in gdf.iterrows():
+        print(point)
+        print(point.within(valor[column_geom]))
+        if point.within(valor[column_geom]):
+            return valor[col_name]
+    return None
+
 # Sin mediciones:
-# Cromo hexavalente
-# Aldrín  
-# DDT (Total Isómeros)
-# Heptacloro
-# Heptacloro epóxido
-# Metoxicloro
-# Paration
-# Malation
-# 2,4 D
+    # Cromo hexavalente
+    # Aldrín  
+    # DDT (Total Isómeros)
+    # Heptacloro
+    # Heptacloro epóxido
+    # Metoxicloro
+    # Paration
+    # Malation
+    # 2,4 D
 
 # Import data
 data_mediciones = pd.read_csv("acumar_mediciones.csv")
 data_mediciones['Valor (=)'] = data_mediciones['Valor (=)'].str.replace('"', '',regex=True).replace(',', '.',regex=True).astype(float)
 data_mediciones['Fecha'] = pd.to_datetime(data_mediciones['Fecha'], format = "%d/%m/%Y")
+data_mediciones['Longitud'] = data_mediciones['Longitud'].str.replace(',', '.').astype(float)
+data_mediciones['Latitud'] = data_mediciones['Latitud'].str.replace(',', '.').astype(float)
 data_limites_uso = pd.read_csv("limites_admisibles_uso.csv")
-limites_subcuencas = pd.read_csv("LIMITES_SUBCUENCAS_.csv")
+
+# Load data
+data_limites_subcuencas = gpd.read_file('LIMITES_SUBCUENCAS_.csv')
+data_limites_cuencas = gpd.read_file('TRAMOS_DE_CUENCA.csv')
 
 # Apply mapping
 data_mediciones['Medida'] = data_mediciones['Medida'].apply(standardize_measurement)
-data_mediciones['Cumple Limites Ia'] = data_mediciones.apply(lambda row: check_condition(row, "I a"), axis=1)
-data_mediciones['Cumple Limites Ib'] = data_mediciones.apply(lambda row: check_condition(row, "I b"), axis=1)
-data_mediciones['Cumple Limites II'] = data_mediciones.apply(lambda row: check_condition(row, "II"), axis=1)
-data_mediciones['Cumple Limites III'] = data_mediciones.apply(lambda row: check_condition(row, "III"), axis=1)
-data_mediciones['Cumple Limites IV'] = data_mediciones.apply(lambda row: check_condition(row, "IV"), axis=1)
+data_mediciones['Cumple Limites Ia'] = data_mediciones.apply(lambda row: check_dummie_condition(row, "I a"), axis=1)
+data_mediciones['Cumple Limites Ib'] = data_mediciones.apply(lambda row: check_dummie_condition(row, "I b"), axis=1)
+data_mediciones['Cumple Limites II'] = data_mediciones.apply(lambda row: check_dummie_condition(row, "II"), axis=1)
+data_mediciones['Cumple Limites III'] = data_mediciones.apply(lambda row: check_dummie_condition(row, "III"), axis=1)
+data_mediciones['Cumple Limites IV'] = data_mediciones.apply(lambda row: check_dummie_condition(row, "IV"), axis=1)
+
+# Convert geometries
+data_limites_subcuencas['geometry_subcuencas'] = data_limites_subcuencas['wkb_geometry'].apply(lambda x: loads(x))
+data_limites_cuencas['geometry_cuencas'] = data_limites_cuencas['wkb_geometry'].apply(lambda x: loads(x))
+
+# Create GeoDataFrames
+data_mediciones['geometry'] = data_mediciones.apply(lambda row: Point(row['Longitud'], row['Latitud']), axis=1)
+data_mediciones = gpd.GeoDataFrame(data_mediciones, geometry='geometry')
+geo_df_subcuencas = gpd.GeoDataFrame(data_limites_subcuencas, geometry='geometry_subcuencas')
+geo_df_cuencas = gpd.GeoDataFrame(data_limites_cuencas, geometry='geometry_cuencas')
+
+data_mediciones['Subcuenca'] = data_mediciones['geometry'].apply(lambda x: geomapping(x, geo_df_subcuencas, 'geometry_subcuencas', 'ID'))
+data_mediciones['Cuenca'] = data_mediciones['geometry'].apply(lambda x: geomapping(x, geo_df_cuencas, 'geometry_cuencas', 'TRAMO'))
 
 # Save to CSV
-data_mediciones.to_csv("data_mediciones_con_dummies.csv", index = False)
+data_mediciones.to_csv("data_mediciones_geolocaliazadas_dummies.csv", index = False)
